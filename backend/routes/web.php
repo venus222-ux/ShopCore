7ea3afcfc2ddf5cf0bf305dc28fe18629806cc90
Stefania Ionescu <stefanya_yo_18@yahoo.com>
2026-08-18 18:51:10 +0300

    Add Octane to the project

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,7 +3,27 @@
 use App\Http\Controllers\MetricsController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/test-worker', function () {
+    if (!isset($GLOBALS['worker_start_time'])) {
+        $GLOBALS['worker_start_time'] = microtime(true);
+        $GLOBALS['worker_counter'] = 0;
+    }
+
+    $GLOBALS['worker_counter']++;
+
+    return response()->json([
+        'numar_vizite_worker' => $GLOBALS['worker_counter'],
+        'process_id' => getmypid(),
+        'worker_boot_time' => $GLOBALS['worker_start_time'],
+    ]);
+});
+
 Route::get('/metrics', [MetricsController::class, 'index']);
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
